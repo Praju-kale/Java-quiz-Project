4ef59807352ff7014e310e3b9c27792a6c747d47
prajakta kale <126392431+Praju-kale@users.noreply.github.com>
2024-01-03 15:05:47 +0530

    Add files via upload

---
+++ basic java quiz.java
@@ -0,0 +1,57 @@
+package capabl;
+      
+public class Basic {
+	
+	public static void main(String[] args) {
+	  System.out.println("Welcome to JAVA Quiz game");
+	  System.out.println("In this quiz we cover the basic concept of java ");
+	  System.out.println("1.Introduction to java");
+	  System.out.println("2.What is JDK,JVM,JRE");
+	  System.out.println("3.Datatypes and Code flow");
+	  System.out.println("4.Decision making Statements");
+	  System.out.print("Que 1 :");
+	  System.out.println("What does JRE stands for?");
+	  System.out.print("options:");
+	  System.out.println("1)JAVA RUNTIME ENVIRONMENT");
+	  System.out.println("2)JAVA RUNABLE ENVIRONMENT");
+	  System.out.println("3)JAVA RUNTIME BEHAVOIR");
+	  int option=1;
+	  switch(option){
+	  case 1:System.out.println("correct answer");
+	  break;
+	  case 2:System.out.println("wrong answer");
+	  break;
+	  case 3:System.out.println("wrong answer");
+	  break;
+	  };
+	  System.out.print("Que 2 :");
+	  System.out.println("What will be output of given program");
+	  System.out.println(" int a=20; int b=10;a is greater than b or not ?");
+	  System.out.println("1)yes");
+	  System.out.println("2)no");
+	  switch(option){
+	  case 1:System.out.println("wrong answer");
+	  break;
+	  case 2:System.out.println("correct answer");
+	  break;
+	  }
+	  System.out.print("Que 3 :");
+      System.out.println(" int age=20");
+      System.out.println("if(age>18){"); 
+      System.out.print("system.out.println");
+      System.out.println("(eligible for vote);}");
+      System.out.print("{System.out.println)}");
+      System.out.println("(not eligible)");
+      System.out.println("1)eligible for vote");
+	  System.out.println("2)not eligible");
+	  System.out.println("3)18");
+	  switch(option){
+	  case 1:System.out.println("correct answer");
+	  break;
+	  case 2:System.out.println("wrong answer");
+	  break;
+	  case 3:System.out.println("wrong answer");
+	  break;
+
+      
+	  }}}
